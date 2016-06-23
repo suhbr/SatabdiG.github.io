0ba1629f20cef9eaa04cc1406c7cd2e18143b097
Satabdi Ganguly <satabdi.ganguly89@gmail.com>
2016-06-23 12:00:25 +0200

    Making changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 //Name:Main JS skeleton
 //version 1.0
 
-var count=0;
+function myfunc()
+{
+    ga('send','event','Submit button','click','buttonclick');
+    var emailid=$("#email").val();
+
+    if(emailid != '')
+    {
+        if(!isValidEmailAddress(emailid))
+            {
+                 $("#textnow").css("color","#ff4d4d");
+                $("#textnow").text("Please enter a valid email address");
+
+            }
+        else
+        {
+             $("#textnow").css("color","#8181F7");
+            $("#textnow").text("Thank you for the message");
+             ga('send','event','Submit done','click','buttonclick');
+            $("#myform").submit();
+
+        }
+
+    }
+    else
+    {
+         console.log(emailid);
+          $("#textnow").css("color","#ff4d4d");
+         $("#textnow").text("Please enter a email id");
+    }
+
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
 
 var tripApp = angular.module('tripApp', ['ngRoute']);
 
@@ -31,10 +66,11 @@ var tripApp = angular.module('tripApp', ['ngRoute']);
 
 
 
-
      // View Controller
     tripApp.controller('mainController', function($scope) {
-      $scope.message = 'In Home Controller';
+      $scope.message = '';
+
+
       });
 
 
